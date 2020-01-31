f7a16ff36246ae8846cf55458fb9d79365d04c47
wings <eternity.mr8@gmail.com>
2020-01-31 22:15:30 +0330

    finish writing test for json place holder package.

--- public/index.php
+++ public/index.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Main app
+ *
+ * PHP version 7.2
+ *
+ * @category Index
+ * @package  Task1
+ * @author   Wings <eternity.mr8@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/eternity-wing/level_up_task1
+ */
+
 declare(strict_types=1);
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
@@ -6,6 +18,11 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 use App\JsonPlaceHolder;
 use App\HtmlPurifier;
 
+/**
+ * Bootstrapping app
+ *
+ * @return void
+ */
 function main():void
 {
     $rawHtml = readline("Enter text: ");
--- tests/JsonPlaceHolderTest.php
+++ tests/JsonPlaceHolderTest.php
@@ -1,20 +1,80 @@
 <?php
+/**
+ * JsonPlaceHolder Unit Test
+ *
+ * PHP version 7.2
+ *
+ * @category JsonPlaceHolderTest
+ * @package  Task1
+ * @author   Wings <eternity.mr8@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/eternity-wing/level_up_task1
+ */
+
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use App\JsonPlaceHolder;
+use App\HtmlPurifier;
+use GuzzleHttp\Client;
 
+/**
+ * HtmlPurifierTest Test Class
+ *
+ * @category UnitTest
+ * @package  Task1
+ * @author   Wings <eternity.mr8@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/eternity-wing/level_up_task1
+ */
 final class JsonPlaceHolderTest extends TestCase
 {
     /**
-     * Empty test
+     * Initializing propery and setting up test needles
      *
      * @return void
      */
-    public function testShouldDoNothing(): void
+    public function setUp():void
     {
-        $this->assertTrue(
-            true,
-            "It should be true"
-        );
+        $this->jsonPlaceHolder = new JsonPlaceHolder();
+        $this->client = new Client(['base_uri' => JsonPlaceHolder::BASE_URI]);
+        $this->purifier = new HtmlPurifier();
+    }
+
+    /**
+     * Destroying propery and setting down test needles
+     *
+     * @return void
+     */
+    public function tearDown():void
+    {
+        unset($this->jsonPlaceHolder);
+    }
+
+    /**
+     * Test for connection
+     *
+     * @return void
+     */
+    public function testIfRemoteServerIsAccessible(): void
+    {
+        $response = $this->client->get('/todos/1');
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    /**
+     * Test for connection
+     *
+     * @return void
+     */
+    public function testStoringOnRemoteStorage(): void
+    {
+        $rawHtml = file_get_contents('tests/fixtures/raw.html');
+        $purifiedHtml = $this->purifier->purify($rawHtml);
+        $result = $this->jsonPlaceHolder->store($purifiedHtml);
+
+        $this->assertArrayHasKey('id', $result);
+        $this->assertEquals($result['id'], 101);
     }
 }
